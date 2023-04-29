bfef246fb777961d7cc79e560795047ec41bb247
Alessandro Silvestri <103107872+Alessandro-Silvestri@users.noreply.github.com>
2023-04-29 14:15:29 +0100

    Create bingo.py

---
+++ bingo.py
@@ -0,0 +1,49 @@
+import random
+
+def pretty_print(t):
+    t = str(t)
+    if len(t) == 1:
+        return f'0{t}'
+    return t
+
+def show_card():
+    print(f'''
+    {bingo_nums[0]}  |   {bingo_nums[1]}  | {bingo_nums[2]}
+    ----+-------+----
+    {bingo_nums[3]}  | Bingo | {bingo_nums[4]}
+    ----+-------+----
+    {bingo_nums[5]}  |   {bingo_nums[6]}  | {bingo_nums[7]}
+    ''')
+
+def check_win(x: list):
+    win = True
+    for i in x:
+        if i.strip() != 'X':
+            win = False
+            break
+    return win
+
+all_numbers = [i for i in range(1, 91)]
+random.shuffle(all_numbers)
+bingo_nums = list(map(pretty_print, all_numbers[0:8]))
+
+while True:
+    show_card()
+    num_call = input('What number was called: ')
+    if num_call in bingo_nums:
+        bingo_nums = list(map(lambda x: x.replace(num_call, ' X'), bingo_nums)) 
+    if check_win(bingo_nums):
+        print('BINGO!!')
+        break
+
+
+
+
+
+
+
+
+
+
+
+
